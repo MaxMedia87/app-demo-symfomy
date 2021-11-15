c0d6b089363958f5e14c18848363d1b06ce1ada5
Maxim <maksim_saharov@mail.ru>
2021-11-15 23:05:48 +0300

    [feat] Реализован базовый пример контроллера. Разбивка на шаблоны

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,27 +4,42 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage(): Response
     {
-        return new Response('Главная страница');
+        return $this->render('homepage.html.twig');
     }
 
     /**
-     * @Route ("/articles/{slug}")
+     * @Route ("/articles/{slug}", name="app_article_show")
      * @param $slug
+     * @return Response
      */
     public function show($slug): Response
     {
-        return new Response(
-            sprintf('Это страница %s', $slug)
+        $comments = [
+            'Crescere etiam ducunt ad teres fraticinida.',
+            'Boil six chocolates, sauerkraut, and butterscotch in a large plastic bag over medium heat, cook for two minutes and season with some steak.',
+            'Boil six chocolates, sauerkraut, and buttersc.',
+            'God theres nothing like the scurvy endurance growing on the woodchuck.'
+        ];
+
+        $slug = str_replace('-', ' ', $slug);
+
+        return $this->render(
+            'articles/show.html.twig',
+            [
+                'article' => ucfirst($slug),
+                'comments' => $comments
+            ]
         );
     }
 }
