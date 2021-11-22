105e07247086bcd9752ed0961304824dd74da8ec
Maxim <maksim_saharov@mail.ru>
2021-11-22 22:11:14 +0300

    [feat] Настроен вывод контента через сервис Markdown Parser

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Demontpx\ParsedownBundle\Parsedown;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,9 +22,10 @@ class ArticleController extends AbstractController
     /**
      * @Route ("/articles/{slug}", name="app_article_show")
      * @param $slug
+     * @param Parsedown $parsed
      * @return Response
      */
-    public function show($slug): Response
+    public function show($slug, Parsedown $parsed): Response
     {
         $comments = [
             'Crescere etiam ducunt ad teres fraticinida.',
@@ -34,11 +36,28 @@ class ArticleController extends AbstractController
 
         $slug = str_replace('-', ' ', $slug);
 
+        $articleContent = <<<EOF
+            **Жили-были на свете три поросенка**. [Три брата](/). Все одинакового роста, кругленькие, розовые, 
+            с одинаковыми веселыми хвостиками. Даже имена у них были похожи. Звали поросят: Ниф-Ниф, Нуф-Нуф и Наф-Наф.
+            Все лето поросята кувыркались в зеленой траве, грелись на солнышке, нежились в лужах. Но вот наступила осень.
+            * Пора нам подумать о зиме, — сказал как-то Наф-Наф своим братьям, проснувшись рано утром. 
+            
+            * Я весь дрожу от холода. Давайте построим дом и будем зимовать вместе под одной теплой крышей.
+            
+            #####Но его братья не хотели браться за работу.
+            
+            * Успеется! До зимы еще далеко. Мы еще погуляем, — сказал Ниф-Ниф и перекувырнулся через голову.
+            
+            * Когда нужно будет, я сам построю себе дом, — сказал Нуф-Нуф и лег в лужу.
+            
+            * Я тоже, — добавил Ниф-Ниф.
+            EOF;
         return $this->render(
             'articles/show.html.twig',
             [
                 'article' => ucfirst($slug),
-                'comments' => $comments
+                'comments' => $comments,
+                'articleContent' => $parsed->text($articleContent)
             ]
         );
     }
