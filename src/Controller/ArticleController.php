c4f90715057c12a1c48ddcf71a999b06e0bfd2b5
Maxim <maksim_saharov@mail.ru>
2022-02-01 23:08:12 +0300

    [feat] Реализован вывод статей из базы данных

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Service\SlackClient;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,10 +24,21 @@ class ArticleController extends AbstractController
     /**
      * @Route ("/articles/{slug}", name="app_article_show")
      * @param $slug
+     * @param EntityManagerInterface $em
+     *
      * @return Response
      */
-    public function show($slug): Response
+    public function show($slug, EntityManagerInterface $em): Response
     {
+        $repository = $em->getRepository(Article::class);
+        $article = $repository->findOneBy(['slug' => $slug]);
+
+        if (null === $article) {
+            throw $this->createNotFoundException(
+                sprintf('Статья %s не найдена', $slug)
+            );
+        }
+
         $comments = [
             'Crescere etiam ducunt ad teres fraticinida.',
             'Boil six chocolates, sauerkraut, and butterscotch in a large plastic bag over medium heat, cook for two minutes and season with some steak.',
@@ -33,31 +46,11 @@ class ArticleController extends AbstractController
             'God theres nothing like the scurvy endurance growing on the woodchuck.'
         ];
 
-        $slug = str_replace('-', ' ', $slug);
-
-        $articleContent = <<<EOF
-**Жили-были на свете три поросенка**. [Три брата](/). Все одинакового роста, кругленькие, розовые, 
-с одинаковыми веселыми хвостиками. Даже имена у них были похожи. Звали поросят: Ниф-Ниф, Нуф-Нуф и Наф-Наф.
-Все лето поросята кувыркались в зеленой траве, грелись на солнышке, нежились в лужах. Но вот наступила осень.
-* Пора нам подумать о зиме, — сказал как-то Наф-Наф своим братьям, проснувшись рано утром. 
-
-* Я весь дрожу от холода. Давайте построим дом и будем зимовать вместе под одной теплой крышей.
-
-#####Но его братья не хотели браться за работу.
-
-* Успеется! До зимы еще далеко. Мы еще погуляем, — сказал Ниф-Ниф и перекувырнулся через голову.
-
-* Когда нужно будет, я сам построю себе дом, — сказал Нуф-Нуф и лег в лужу.
-
-* Я тоже, — добавил Ниф-Ниф.
-EOF;
-
         return $this->render(
             'articles/show.html.twig',
             [
-                'article' => ucfirst($slug),
+                'article' => $article,
                 'comments' => $comments,
-                'articleContent' => $articleContent
             ]
         );
     }
