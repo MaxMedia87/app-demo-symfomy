602db394eddb4a456835b1424351ad91edfdbeba
Maxim <maksim_saharov@mail.ru>
2022-02-11 22:52:08 +0300

    [feat] Реализован класс фикстуры статей

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use DateTimeImmutable;
+use Doctrine\Persistence\ObjectManager;
+
+class ArticleFixtures extends BaseFixtures
+{
+    private static $titles = [
+        'Есть ли жизнь после девятой жизни?',
+        'Когда в машинах поставят лоток?',
+        'В погоне за красной точкой',
+        'В чем смысл жизни сосисок',
+    ];
+
+
+    private static $authors = [
+        'Кексик',
+        'Матроскин',
+        'Фунтик',
+        'Колбаска',
+        'Беляш'
+    ];
+
+    private static $images = [
+        'car1.jpg',
+        'car2.jpg',
+        'car3.jpg'
+    ];
+
+    public function loadData(ObjectManager $manager): void
+    {
+        $this->createMany(Article::class, 10, function (Article $article) {
+            $article
+                ->setTitle($this->faker->randomElement(self::$titles))
+                ->setSlug($this->faker->slug())
+                ->setBody(
+                    '**Жили-были на свете три поросенка**. [Три брата](/).' .
+                    'Все одинакового роста, кругленькие, розовые, с одинаковыми веселыми хвостиками. ' .
+                    'Даже имена у них были похожи. Звали поросят: Ниф-Ниф, Нуф-Нуф и Наф-Наф. ' .
+                    $this->faker->paragraphs($this->faker->numberBetween(2, 5), true)
+                );
+            if ($this->faker->boolean(60)) {
+                $article->setPublishedAt(
+                    new DateTimeImmutable(
+                        $this->faker->dateTimeBetween('-100 days')->format('Y-m-d h:i:s')
+                    )
+                );
+            }
+
+            $article
+                ->setAuthor($this->faker->randomElement(self::$authors))
+                ->setLikeCount($this->faker->numberBetween(0, 10))
+                ->setImageFileName($this->faker->randomElement(self::$images));
+        });
+    }
+}
