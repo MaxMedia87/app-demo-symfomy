4f92a36a768e599f058b77397da8342bf988f257
Maxim <maksim_saharov@mail.ru>
2022-03-06 21:41:35 +0300

    [feat] Реализована авторизация на сайте

---
+++ src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Security;
+
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
+
+class LoginFormAuthenticator extends AbstractAuthenticator
+{
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    public function __construct(RouterInterface $router, UserRepository $userRepository)
+    {
+        $this->router = $router;
+        $this->userRepository = $userRepository;
+    }
+
+    public function supports(Request $request): ?bool
+    {
+        return $request->attributes->get('_route') === 'app_login'
+            && $request->isMethod('POST');
+    }
+
+    public function authenticate(Request $request): PassportInterface
+    {
+        $email = $request->get('email');
+        $password = $request->get('password');
+
+        $request->getSession()->set(Security::LAST_USERNAME, $email);
+
+        return new Passport(
+            new UserBadge($email,
+                function ($userIdentifier) {
+                    $user = $this->userRepository->findOneBy(['email' => $userIdentifier]);
+
+                    if (null === $user) {
+                        throw new UserNotFoundException('Пользователь не найден.');
+                    }
+
+                    return $user;
+                }
+            ),
+            new PasswordCredentials($password)
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        return new RedirectResponse(
+            $this->router->generate('app_homepage')
+        );
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
+
+        return new RedirectResponse(
+            $this->router->generate('app_login')
+        );
+    }
+
+//    public function start(Request $request, AuthenticationException $authException = null): Response
+//    {
+//        /*
+//         * If you would like this class to control what happens when an anonymous user accesses a
+//         * protected page (e.g. redirect to /login), uncomment this method and make this class
+//         * implement Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface.
+//         *
+//         * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
+//         */
+//    }
+}
