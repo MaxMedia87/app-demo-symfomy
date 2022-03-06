9e6d23cd9c22a9118b441f9c1f1a34d7f4ab3af9
Maxim <maksim_saharov@mail.ru>
2022-03-06 22:34:35 +0300

    [feat] Реализовано хеширование пароля

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -4,16 +4,27 @@ namespace App\DataFixtures;
 
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends BaseFixtures
 {
+    /**
+     * @var UserPasswordHasherInterface
+     */
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function loadData(ObjectManager $manager): void
     {
         $this->createMany(User::class, 10, function (User $user) {
             $user
                 ->setEmail($this->faker->email)
                 ->setFirstName($this->faker->firstName())
-                ->setPassword('123456');
+                ->setPassword($this->passwordHasher->hashPassword($user, '123456'));
         });
     }
 }
