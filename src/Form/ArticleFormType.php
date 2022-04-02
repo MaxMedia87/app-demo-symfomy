07af6ab09d934347bc53b668356c276df4ee8a2a
Maxim <maksim_saharov@mail.ru>
2022-04-02 21:31:47 +0300

    [feat] Рассмотрены типы полей и трансформация данных

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +15,28 @@ class ArticleFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('body')
+            ->add('title', TextType::class, [
+                'label' => 'Укажите название статьи',
+                'help' => 'Не используйте слово "собака"',
+            ])
+            ->add('body', TextareaType::class, [
+                'label' => 'Содержимое статьи'
+            ])
+            ->add('publishedAt', null, [
+                'label' => 'Дата публикации',
+                'widget' => 'single_text'
+            ])
         ;
+
+        $builder->get('body')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($bodyTransformFromDatabase) {
+                    return str_replace('**собака**', 'собака', $bodyTransformFromDatabase);
+                },
+                function ($bodyFromInput) {
+                    return str_replace('собака', '**собака**', $bodyFromInput);
+                }
+            ));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
