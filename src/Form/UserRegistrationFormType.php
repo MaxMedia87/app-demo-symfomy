b6bb8840e7a68e4a732ae58ac0e4a88932c9d24d
Maxim <maksim_saharov@mail.ru>
2022-04-28 21:59:21 +0300

    Добавлен промежуточный слой DTO для регистрации пользователя - Перенесена валидация полей формы - Изменен объект формы c User на DTO

--- src/Form/UserRegistrationFormType.php
+++ src/Form/UserRegistrationFormType.php
@@ -2,59 +2,30 @@
 
 namespace App\Form;
 
-use App\Entity\User;
-use App\Form\Validator\IsNotSpamBot;
+use App\Form\Model\UserRegistrationFormModel;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserRegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class, [
-                'required' => false,
-                'constraints' => [
-                    new IsNotSpamBot()
-                ]
-            ])
+            ->add('email', EmailType::class)
             ->add('firstName')
-            ->add('plainPassword', PasswordType::class, [
-                'mapped' => false,
-                'required' => false,
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Пароль не указан.'
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Пароль должен быть не менее 6 символов'
-                    ])
-                ]
-            ])
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'required' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'Подтвердите согласие на обработку',
-                    ])
-                ]
-            ])
+            ->add('plainPassword', PasswordType::class)
+            ->add('agreeTerms', CheckboxType::class)
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => User::class,
+            'data_class' => UserRegistrationFormModel::class,
         ]);
     }
 }
